b9b1f51fda600172624d331165cb2c0cde6ddaa9
Tushar Gupta <tushargupta8954@gmail.com>
2026-04-21 17:26:15 +0530

    Learned and implemented Context API

---
+++ mini-context/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import UserContextProvider from './context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>React with Hitesh sir</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
